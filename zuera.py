ae1feb19b693e6d2fc85361e2a839b5eb8dcbeb9
falamansa <fighter3215@gmail.com>
2023-08-09 20:20:05 -0300

    novaera

--- zuera.py
+++ zuera.py
@@ -1,1 +1,7 @@
-print("rodrigo")
+import time
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import json
